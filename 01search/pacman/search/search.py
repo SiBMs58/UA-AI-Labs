90867039981ec0ef3bf1be50bfb897bab0791706
Siebe Mees <siebe.mees@student.uantwerpen.be>
2025-10-03 19:12:56 +0200

    02. FEAT: q1

--- 01search/pacman/search/search.py
+++ 01search/pacman/search/search.py
@@ -86,8 +86,29 @@ def depthFirstSearch(problem: SearchProblem):
     print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
     print("Start's successors:", problem.getSuccessors(problem.getStartState()))
     """
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    # Border case where start state is the goal state
+    if problem.isGoalState(problem.getStartState()):
+        return problem.getStartState()
+
+    # General case of finiding optimal path
+    fringe = util.Stack() # Keep track the fringe in the case of depthfirst its of a stack containing (state, path)
+    fringe.push((problem.getStartState(), [])) # Start withe pushing the start staete to the fringe
+    visited = [] # Keep a list of visited stated else we could have a deadlock/infite loop
+    while not fringe.isEmpty():
+        state, path = fringe.pop()
+
+        if state in visited: # Check if we already visited
+            continue
+        visited.append(state)
+
+        if problem.isGoalState(state): # If this state is the goal state we get the path corrresponding tot the state
+            return path
+
+        for successor, action, step_cost in problem.getSuccessors(state): # Then we have a look at its successors, along wiht tther cost and actions
+            if successor not in visited: # check if we already visited the succesoor
+                fringe.push((successor, path + [action])) # if not, we push the tuple to the frigne
+
+    return []
 
 def breadthFirstSearch(problem: SearchProblem):
     """Search the shallowest nodes in the search tree first."""
