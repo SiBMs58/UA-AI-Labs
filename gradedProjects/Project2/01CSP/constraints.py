20f5266d89e3008d6990621f2eb12da12a4fbe1e
Siebe Mees <siebe.mees@student.uantwerpen.be>
2025-11-06 15:26:01 +0100

    Project 02. FEAT: CSP Futoshiki

--- gradedProjects/Project2/01CSP/constraints.py
+++ gradedProjects/Project2/01CSP/constraints.py
@@ -29,6 +29,38 @@ def add_constraints(model: "cp_model.CpModel", X, parsed):
     horiz  = parsed["horiz"]
     vert   = parsed["vert"]
 
-    # TODO: add the constrained on the model
+    # 1) Row and column AllDifferent
+    for r in range(N):
+        model.AddAllDifferent(X[r])
 
-    pass
+    for c in range(N):
+        col_vars = [X[r][c] for r in range(N)]
+        model.AddAllDifferent(col_vars)
+
+    # 2) Givens (prefilled digits)
+    for r in range(N):
+        for c in range(N):
+            if givens[r][c] is not None:
+                model.Add(X[r][c] == givens[r][c])
+
+    # 3) Inequalities: Horizontal inequalities
+    # horiz[(r, c)] relates X[r][c] ? X[r][c+1]
+    for (r, c), sign in horiz.items():
+        if sign == "<":
+            model.Add(X[r][c] < X[r][c + 1])
+        elif sign == ">":
+            model.Add(X[r][c] > X[r][c + 1])
+        else:
+            raise ValueError(f"Unknown horizontal sign {sign} at {(r, c)}")
+
+    # 3) Inequalities: Vertical inequalities
+    # vert[(r, c)] relates X[r][c] ? X[r+1][c]
+    # '^' means TOP < BOTTOM
+    # 'v' means TOP > BOTTOM
+    for (r, c), sign in vert.items():
+        if sign == "^":
+            model.Add(X[r][c] < X[r + 1][c])
+        elif sign == "v":
+            model.Add(X[r][c] > X[r + 1][c])
+        else:
+            raise ValueError(f"Unknown vertical sign {sign} at {(r, c)}")
